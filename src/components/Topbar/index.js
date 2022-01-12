5f90c29c7ea73f693d248599d3fd06fff83d8bac
Maiku <maiku00@my.yorku.ca>
2022-01-11 23:07:26 -0500

    Code refactoring

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -3,7 +3,7 @@ import { Box, Heading } from '@chakra-ui/react';
 const Topbar = () => {
     return (
         <Box p={3} bg="transparent">
-            <Heading as="h1" size="lg" color="white">Spacetagram.</Heading>
+            <Heading as="h1" size="lg" color="white">Spacestagram.</Heading>
         </Box>
     );
 }
